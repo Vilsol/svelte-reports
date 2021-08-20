b4a01d9a9e8ac356474e9eb9b1906854ecc67a4d
Vilsol <me@vil.so>
2021-08-20 08:13:59 +0300

    add routing issue

---
+++ routing/src/routes/test/[hello]/index.svelte
@@ -0,0 +1,13 @@
+<script lang="ts" context="module">
+  export const load = (input) => ({
+    props: {
+      ...(input.page.params || {}),
+    },
+  });
+</script>
+
+<script lang="ts">
+  export let hello!: string;
+</script>
+
+<h1>{hello}</h1>
